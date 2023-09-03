536093a3de5849cc0facb6a3abcd50a41b53f3c9
Mark Wainwright <wainwrightml@gmail.com>
2023-09-03 18:40:58 +0100

    bug fix to transition_value

--- src/transition/transition_value.rs
+++ src/transition/transition_value.rs
@@ -3,20 +3,21 @@ pub use super::prelude::*;
 use bevy::prelude::*;
 
 impl<'c, 'w, 's, 'a, 'world> ComponentCommands<'c, 'w, 's, 'a, 'world> {
-    /// Inserts a transition
+    /// Inserts a transition to a particular destination
     pub fn transition_value<L: Lens + GetValueLens>(
         &mut self,
         default_initial_value: L::Value,
         destination: L::Value,
         speed: Option<<L::Value as Tweenable>::Speed>,
-    ) -> L::Value
-    where
+    ) where
         L::Value: Tweenable,
         L::Object: Clone + Component,
     {
-        let current_value = match self.get::<L::Object>() {
-            Some(o) => L::try_get_value(o).unwrap_or(default_initial_value),
-            None => default_initial_value,
+        let Some(current_value) = self.get::<L::Object>().and_then(L::try_get_value) else {
+            self.insert(Transition::<L> {
+                step: TransitionStep::new_arc(default_initial_value, None, None),
+            });
+            return;
         };
 
         if let Some(previous_path) = self.get::<Transition<L>>() {
@@ -24,21 +25,19 @@ impl<'c, 'w, 's, 'a, 'world> ComponentCommands<'c, 'w, 's, 'a, 'world> {
                 && previous_path.step.speed == speed
                 && previous_path.step.next.is_none()
             {
-                return current_value; //previous path is the same - do not replace
+                return;
             }
 
             if current_value.eq(&destination) {
                 self.remove::<Transition<L>>();
-                return current_value;
+                return;
             }
         } else if current_value.eq(&destination) {
-            return current_value;
+            return;
         }
 
         self.insert(Transition::<L> {
             step: TransitionStep::new_arc(destination, speed, None),
         });
-
-        current_value
     }
 }
