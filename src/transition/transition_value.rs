1281eba2a0a8260af895d0b24fbef0a3162ef87c
Mark Wainwright <wainwrightml@gmail.com>
2023-09-03 18:47:42 +0100

    changed transition_value

--- src/transition/transition_value.rs
+++ src/transition/transition_value.rs
@@ -3,21 +3,23 @@ pub use super::prelude::*;
 use bevy::prelude::*;
 
 impl<'c, 'w, 's, 'a, 'world> ComponentCommands<'c, 'w, 's, 'a, 'world> {
-    /// Inserts a transition to a particular destination
+    /// Inserts a transition to a particular
     pub fn transition_value<L: Lens + GetValueLens>(
         &mut self,
         default_initial_value: L::Value,
         destination: L::Value,
         speed: Option<<L::Value as Tweenable>::Speed>,
-    ) where
-        L::Value: Tweenable,
+    )
+        -> L::Value
+     where
+        L::Value: Tweenable + Clone,
         L::Object: Clone + Component,
     {
         let Some(current_value) = self.get::<L::Object>().and_then(L::try_get_value) else {
             self.insert(Transition::<L> {
-                step: TransitionStep::new_arc(default_initial_value, None, None),
+                step: TransitionStep::new_arc(default_initial_value.clone(), None, None),
             });
-            return;
+            return default_initial_value;
         };
 
         if let Some(previous_path) = self.get::<Transition<L>>() {
@@ -25,19 +27,21 @@ impl<'c, 'w, 's, 'a, 'world> ComponentCommands<'c, 'w, 's, 'a, 'world> {
                 && previous_path.step.speed == speed
                 && previous_path.step.next.is_none()
             {
-                return;
+                return current_value;
             }
 
             if current_value.eq(&destination) {
                 self.remove::<Transition<L>>();
-                return;
+                return current_value;
             }
         } else if current_value.eq(&destination) {
-            return;
+            return current_value;
         }
 
         self.insert(Transition::<L> {
             step: TransitionStep::new_arc(destination, speed, None),
         });
+
+        return current_value;
     }
 }
