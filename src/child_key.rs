4d6d07a01057b90d7731105b4b1c148456c5d90c
Mark Wainwright <wainwrightml@gmail.com>
2023-12-08 12:04:38 +0000

    added more child_key options

--- src/child_key.rs
+++ src/child_key.rs
@@ -1,8 +1,29 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum ChildKey {
-    Number(u32),
-    String(&'static str),
+pub enum NumberKey{
+    Unsigned(u32),
+    Signed(i32),
+    Pair(u16,u16),
+    Trio(u16,u16,u16)
 }
+
+impl std::fmt::Display for NumberKey {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Unsigned(u) => u.fmt(f),
+            Self::Signed(i) => i.fmt(f),
+            Self::Pair(x,y) => write!(f, "({x}, {y})"),
+            Self::Trio(x,y,z) => write!(f, "({x}, {y}, {z})"),
+        }
+    }
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum ChildKey {
+    String(&'static str),
+    Number(NumberKey),
+}
+
 
 impl std::fmt::Display for ChildKey {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -15,7 +36,27 @@ impl std::fmt::Display for ChildKey {
 
 impl From<u32> for ChildKey {
     fn from(value: u32) -> Self {
-        Self::Number(value)
+        Self::Number(NumberKey::Unsigned(value))
+    }
+}
+
+
+impl From<i32> for ChildKey {
+    fn from(value: i32) -> Self {
+        Self::Number(NumberKey::Signed(value))
+    }
+}
+
+impl From<(u16,u16)> for ChildKey {
+    fn from(value: (u16,u16)) -> Self {
+        Self::Number(NumberKey::Pair(value.0, value.1))
+    }
+}
+
+
+impl From<(u16,u16, u16)> for ChildKey {
+    fn from(value: (u16,u16, u16)) -> Self {
+        Self::Number(NumberKey::Trio(value.0, value.1, value.2))
     }
 }
 
