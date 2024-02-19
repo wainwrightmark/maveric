16eaf9d523682bc3e53955cc105a6da6a4777d5e
Mark Wainwright <wainwrightml@gmail.com>
2024-02-19 13:52:41 +0000

    made image_node and text_node require bevy_ui

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -2,17 +2,24 @@
 pub mod button_node;
 #[cfg(feature="bevy_ui")]
 pub mod carousel;
+#[cfg(feature="bevy_ui")]
 pub mod image_node;
-pub mod text2d_node;
+#[cfg(feature="bevy_ui")]
 pub mod text_node;
+
+pub mod text2d_node;
+
 pub mod sprite_node;
 pub mod prelude {
     #[cfg(feature="bevy_ui")]
     pub use crate::widgets::button_node::*;
     #[cfg(feature="bevy_ui")]
     pub use crate::widgets::carousel::*;
+    #[cfg(feature="bevy_ui")]
     pub use crate::widgets::image_node::*;
+    #[cfg(feature="bevy_ui")]
     pub use crate::widgets::text_node::*;
+    pub use crate::widgets::text2d_node::*;
     pub use crate::widgets::sprite_node::*;
 }
 
