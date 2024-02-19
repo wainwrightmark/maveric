a4ab04009d0f101f8a3d7256bc1368bed5a26c98
Mark Wainwright <wainwrightml@gmail.com>
2024-02-19 13:44:37 +0000

    Allow widgets without bevy_ui

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,11 +1,15 @@
+#[cfg(feature="bevy_ui")]
 pub mod button_node;
+#[cfg(feature="bevy_ui")]
 pub mod carousel;
 pub mod image_node;
 pub mod text2d_node;
 pub mod text_node;
 pub mod sprite_node;
 pub mod prelude {
+    #[cfg(feature="bevy_ui")]
     pub use crate::widgets::button_node::*;
+    #[cfg(feature="bevy_ui")]
     pub use crate::widgets::carousel::*;
     pub use crate::widgets::image_node::*;
     pub use crate::widgets::text_node::*;
