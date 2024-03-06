81b9fff19f97e543e7ac221fab7d8d6cfcecf187
Mark Wainwright <wainwrightml@gmail.com>
2024-03-06 10:56:24 +0000

    added multi_text2d_node

--- src/widgets/multi_text2d_node.rs
+++ src/widgets/multi_text2d_node.rs
@@ -3,28 +3,23 @@ use bevy::{
     text::{Text2dBounds, TextLayoutInfo},
 };
 
+
 pub use crate::prelude::*;
 
 /// A text node in 2d space.
 /// Note that you will need to attach a transform as well
 #[derive(Debug, Clone)]
-pub struct Text2DNode<T: core::fmt::Display + PartialEq + Clone + Send + Sync + 'static> {
-    pub text: T,
-    pub font: &'static str,
-    pub font_size: f32,
-    pub color: Color,
+pub struct MultiText2DNode<const SECTIONS: usize, T: core::fmt::Display + PartialEq + Clone + Send + Sync + 'static> {
+    pub sections: [Option<TextSectionData<T>>; SECTIONS],
     pub justify_text: JustifyText,
     pub linebreak_behavior: bevy::text::BreakLineOn,
     pub text_anchor: Anchor,
     pub text_2d_bounds: Text2dBounds,
 }
 
-impl<T: core::fmt::Display + PartialEq + Clone + Send + Sync + 'static> PartialEq for Text2DNode<T> {
+impl<const SECTIONS: usize, T: core::fmt::Display + PartialEq + Clone + Send + Sync + 'static> PartialEq for MultiText2DNode<SECTIONS, T> {
     fn eq(&self, other: &Self) -> bool {
-        self.text == other.text
-            && self.font == other.font
-            && self.font_size == other.font_size
-            && self.color == other.color
+        self.sections == other.sections
             && self.justify_text == other.justify_text
             && self.linebreak_behavior == other.linebreak_behavior
             && self.text_anchor == other.text_anchor
@@ -32,16 +27,26 @@ impl<T: core::fmt::Display + PartialEq + Clone + Send + Sync + 'static> PartialE
     }
 }
 
+#[derive(Debug, Clone, PartialEq)]
+pub struct TextSectionData<T: core::fmt::Display + PartialEq + Clone + Send + Sync + 'static> {
+    pub text: T,
+    pub font: &'static str,
+    pub font_size: f32,
+    pub color: Color,
+}
+
 fn text_2d_bound_compare(l: &Text2dBounds, r: &Text2dBounds) -> bool {
     l.size == r.size
 }
 
-impl<T: core::fmt::Display + PartialEq + Clone + Send + Sync + 'static> MavericNode for Text2DNode<T> {
+impl<const SECTIONS: usize,T: core::fmt::Display + PartialEq + Clone + Send + Sync + 'static> MavericNode
+    for MultiText2DNode<SECTIONS, T>
+{
     type Context = ();
 
     fn set_components(mut commands: SetComponentCommands<Self, Self::Context>) {
         commands.insert_static_bundle((SpatialBundle::default(), TextLayoutInfo::default()));
-        commands.node_to_bundle(|x|&x.text_anchor);
+        commands.node_to_bundle(|x| &x.text_anchor);
         commands.node_to_component(|x| &x.text_2d_bounds, text_2d_bound_compare);
 
         commands.scope(|commands| {
@@ -52,18 +57,23 @@ impl<T: core::fmt::Display + PartialEq + Clone + Send + Sync + 'static> MavericN
                         let server: &AssetServer = commands
                             .get_res_untracked()
                             .expect("Could not get asset server");
-                        let font = server.load(node.font);
-                        let mut bundle = Text::from_section(
-                            node.text.to_string(),
-                            TextStyle {
-                                font,
-                                font_size: node.font_size,
-                                color: node.color,
-                            },
-                        )
-                        .with_justify(node.justify_text);
 
+                        let mut bundle = Text::default().with_justify(node.justify_text);
                         bundle.linebreak_behavior = node.linebreak_behavior;
+
+                        for section in node.sections.iter().flatten(){
+                            let font = server.load(section.font);
+
+                                let style = TextStyle {
+                                    font,
+                                    font_size: section.font_size,
+                                    color: section.color,
+                                };
+
+                                bundle.sections.push(TextSection { value: section.text.to_string(), style });
+                        }
+
+
                         commands.insert(bundle);
                     }
                 })
